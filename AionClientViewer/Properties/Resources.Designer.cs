ec347ecaddc9477d735a0fda3934c49a3d5631aa
sort <sort@nowhere>
2019-05-08 22:16:30 -0700

    project cleanup

--- AionClientViewer/Properties/Resources.Designer.cs
+++ AionClientViewer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace monono2.AionClientViewer.Properties {
+namespace monono2.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace monono2.AionClientViewer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AionGui.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("monono2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
